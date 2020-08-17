06acd532f4516e0a897012109fe4bcd39b7d9b62
edwardhannan <hannanedward@gmail.com>
2020-08-17 17:19:55 -0400

    setup.py : unfinished, problem with Makefile

--- setup.py
+++ setup.py
@@ -1,28 +1,25 @@
 import os
 import subprocess
+from distutils.core import setup, Extension
 
-# Importing Cython package
-os.system("sudo apt-get install python3-pip")
-os.system("pip install Cython")
 
 # Setting environmental variable NOMAD_HOME with nomad.3.9.1 directory
 path = os.getcwd()
 path_nomad = path + '/nomad.3.9.1'
 os.chdir(path_nomad)
-os.environ['NOMAD_HOME'] = path_nomad
-#os.environ['NOMAD_HOME'] = "/c/Users/Edward/Desktop/E20/GERAD_bbochallenge/nomad.3.9.1"  # Edward
+# os.environ['NOMAD_HOME'] = path_nomad
+os.environ['NOMAD_HOME'] = "/c/Users/Edward/Desktop/E20/GERAD_bbochallenge/nomad.3.9.1"  # Edward
 
-# Run configure and make files
-#subprocess.run(["bash.exe", "configure", "--compiler=/c/MinGW/bin/g++.exe"])  # Edward
+
+# Run shell script configure and make
 subprocess.run(["bash.exe", "configure"])
-os.system("g++  ./Makefile")  # TODO : essayer subprocess.call("g++ ./Makefile"), si fontionne pas
+# os.system("make")
+os.system("C:\\MinGW\\bin\\mingw32-make.exe")  # Edward : simulates the "make" command for Windows
+
 
 # Build PyNomad
 os.chdir(path + '/interface')
-os.environ['NOMAD_HOME_PERSONAL'] = path_nomad  # TODO : pas certain
+os.environ['NOMAD_HOME_PERSONAL'] = path_nomad
 os.system('python setup_PyNomad.py build_ext --inplace')
 
-# Run test and return main directory
-os.system('python runTest.py')
-os.chdir(path)
 
