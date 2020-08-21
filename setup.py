5ca5b9629917c6f9113bf7ed36b27fcd1e78beed
edwardhannan <hannanedward@gmail.com>
2020-08-21 13:09:47 -0400

    setup.py clean Unix version, problem with NOMAD_HOME env var

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 import os
 import subprocess
-from distutils.core import setup, Extension
 
 # Setting environmental variable NOMAD_HOME with nomad.3.9.1 directory
 path = os.getcwd()
 path_nomad = path + '/nomad.3.9.1'
 os.chdir(path_nomad)
-#os.environ['NOMAD_HOME'] = path_nomad  # TODO : fix pathing problem
-os.environ['NOMAD_HOME'] = path_nomad+'//bin'  # manual fix
+os.environ['NOMAD_HOME'] = path_nomad  # TODO : fix pathing problem
+#os.environ['NOMAD_HOME'] = path_nomad+'//bin'  # manual fix
 
 # Run shell script configure and make
 with open('configure', 'r') as file:
     configure_script = file.read()
 subprocess.run(configure_script, shell=True)
 
-os.system("make")
+os.system("make")  # requires gcc compiler
 
 # Build PyNomad
-#os.chdir(path + '/interface')
-#os.environ['NOMAD_HOME_PERSONAL'] = path_nomad
-#os.system('python setup_PyNomad.py build_ext --inplace')
+os.chdir(path + '/interface')
+os.environ['NOMAD_HOME_PERSONAL'] = path_nomad
+os.system('python setup_PyNomad.py build_ext --inplace')
 
 
