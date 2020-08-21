219d76138dedc4e03eccea5e9902edb2749461e9
edwardhannan <hannanedward@gmail.com>
2020-08-20 22:50:37 -0400

    NOMAD_HOME problem to be fixed

--- setup.py
+++ setup.py
@@ -5,16 +5,20 @@ from distutils.core import setup, Extension
 # Setting environmental variable NOMAD_HOME with nomad.3.9.1 directory
 path = os.getcwd()
 path_nomad = path + '/nomad.3.9.1'
-os.environ['NOMAD_HOME'] = path_nomad
 os.chdir(path_nomad)
+#os.environ['NOMAD_HOME'] = path_nomad  # TODO : fix pathing problem
+os.environ['NOMAD_HOME'] = path_nomad+'//bin'  # manual fix
 
 # Run shell script configure and make
-subprocess.run("configure", shell=True)
-os.system("make Makefile")
+with open('configure', 'r') as file:
+    configure_script = file.read()
+subprocess.run(configure_script, shell=True)
+
+os.system("make")
 
 # Build PyNomad
-os.chdir(path + '/interface')
-os.environ['NOMAD_HOME_PERSONAL'] = path_nomad
-os.system('python setup_PyNomad.py build_ext --inplace')
+#os.chdir(path + '/interface')
+#os.environ['NOMAD_HOME_PERSONAL'] = path_nomad
+#os.system('python setup_PyNomad.py build_ext --inplace')
 
 
